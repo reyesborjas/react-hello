b16b5519a1f79860c62933146fc9d0de95071e72
AnaliaMagallanes <analiapiriwindy@gmail.com>
2024-12-18 21:13:52 -0300

    nuevos cambios

--- src/js/component/titulo.jsx
+++ src/js/component/titulo.jsx
@@ -1,4 +1,10 @@
-
+import React from "react"
+export function Titulo(){
+return(
+<div>
 <h1>A Warm Welcome</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni repellat expedita necessitatibus nam. Iure ex ipsa repudiandae fugiat, sed, eum sit corporis nam necessitatibus tempore nostrum, officia animi cumque aut.</p>
 <button>Call to action!</button>
+</div>
+)
+}
