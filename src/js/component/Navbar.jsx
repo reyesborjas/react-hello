64584ff0f9b1c792df74339b0c6e209c5ecdf2be
AnaliaMagallanes <analiapiriwindy@gmail.com>
2024-12-22 18:28:52 -0300

    nuevos cambios

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -4,18 +4,20 @@ import React from "react"
 export function Navbar(){
 return (
 <div>
-<ul className="nav justify-content-end">
+ 
+ 
+  <h5>Start Bootstrap</h5> <ul className="nav justify-content-end">
   <li className="nav-item">
-    <a className="nav-link active" aria-current="page" href="#">Active</a>
+    <a className="nav-link active" aria-current="page" href="#">Home</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="#">Link</a>
+    <a className="nav-link" href="#">About</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="#">Link</a>
+    <a className="nav-link" href="#">Service</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+    <a className="nav-link disabled" aria-disabled="true">Contact</a>
   </li>
 </ul>
 </div>
--- src/js/component/titulo.jsx
+++ src/js/component/titulo.jsx
@@ -1,7 +1,8 @@
 import React from "react"
+import "../../styles/index.css";
 export function Titulo(){
 return(
-<div>
+<div style={{margin: 5}}>
 <h1>A Warm Welcome</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni repellat expedita necessitatibus nam. Iure ex ipsa repudiandae fugiat, sed, eum sit corporis nam necessitatibus tempore nostrum, officia animi cumque aut.</p>
 <button>Call to action!</button>
