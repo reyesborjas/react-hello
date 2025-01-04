2ef2b786410863ee29e2764ac787b59b859ebe94
reyesborjas <reyesborjas@gmail.com>
2025-01-04 11:09:30 -0300

    version final

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -6,7 +6,7 @@ return (
 <div>
  
  
-  <h5>Start Bootstrap</h5> <ul className="nav justify-content-end">
+   <ul className="nav justify-content-end">
   <li className="nav-item">
     <a className="nav-link active" aria-current="page" href="#">Home</a>
   </li>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -18,28 +18,28 @@ function Home() {
 
   const cardData = [
     {
-      image: "https://via.placeholder.com/150",
+      image: "https://soltech.net/wp-content/uploads/2014/05/HTML5.webp",
       title: "Card 1",
       text: "This is the first card.",
       link: "#",
       buttonText: "Learn More",
     },
     {
-      image: "https://via.placeholder.com/150",
+      image: "https://blog.leonhassan.co.uk/content/images/size/w2000/2019/09/css3.svg",
       title: "Card 2",
       text: "This is the second card.",
       link: "#",
       buttonText: "Learn More",
     },
     {
-      image: "https://via.placeholder.com/150",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png",
       title: "Card 3",
       text: "This is the third card.",
       link: "#",
       buttonText: "Learn More",
     },
     {
-      image: "https://via.placeholder.com/150",
+      image: "https://keepcoding.io/wp-content/uploads/2023/08/image-200.png",
       title: "Card 4",
       text: "This is the fourth card.",
       link: "#",
